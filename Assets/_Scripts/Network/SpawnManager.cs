f446a3f8cda6f614f9037dd85cc871e3ed472ea0
Daniel <danielkotik14@gmail.com>
2025-05-02 02:54:11 -0300

    02-May-2025
    fixed water issue and typing to make the water to sync

--- Assets/_Scripts/Network/SpawnManager.cs
+++ Assets/_Scripts/Network/SpawnManager.cs
@@ -1,36 +1,58 @@
 // C#
 using System.Collections;
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.SceneManagement;
 
 public class SpawnManager : NetworkBehaviour
 {
     [SerializeField] private GameObject _playerPrefab;
     [SerializeField] private Transform[] _spawnPoints;
-
     private int _nextSpawnIndex = 0;
+    private bool _sceneLoaded;
+
+    // Track spawned client IDs to avoid duplicate spawns.
+    private HashSet<ulong> _spawnedClients = new HashSet<ulong>();
 
     public override void OnNetworkSpawn()
     {
         if (!IsServer)
             return;
 
-        NetworkManager.Singleton.OnClientConnectedCallback += SpawnForClient;
+        NetworkManager.Singleton.SceneManager.OnSceneEvent += HandleSceneEvent;
+    }
 
-        foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
+    private void HandleSceneEvent(SceneEvent sceneEvent)
+    {
+        if (sceneEvent.SceneEventType == SceneEventType.LoadComplete)
         {
-            SpawnForClient(clientId);
+            _sceneLoaded = true;
+            foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
+            {
+                SpawnForClient(clientId);
+            }
         }
     }
 
     private void SpawnForClient(ulong clientId)
     {
+        // Only spawn if this client hasn't been spawned yet.
+        if (_spawnedClients.Contains(clientId))
+            return;
+
+        // If the scene isn’t loaded, delay the spawn.
+        if (!_sceneLoaded)
+        {
+            StartCoroutine(DelaySpawn(clientId));
+            return;
+        }
+
         Transform spawn = _spawnPoints[_nextSpawnIndex % _spawnPoints.Length];
         _nextSpawnIndex++;
 
         GameObject go = Instantiate(_playerPrefab, spawn.position, spawn.rotation);
-
         var boatMovement = go.GetComponent<StrippedTubBoatMovement>();
         if (boatMovement != null && boatMovement.targetSurface == null)
         {
@@ -40,39 +62,47 @@ public class SpawnManager : NetworkBehaviour
         {
             Debug.LogError("BoatMovement component not found on spawned object.", this);
         }
-
         go.GetComponent<NetworkObject>().SpawnAsPlayerObject(clientId);
+
+        _spawnedClients.Add(clientId);
+    }
+
+    private IEnumerator DelaySpawn(ulong clientId)
+    {
+        while (!_sceneLoaded)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        SpawnForClient(clientId);
     }
 
     private IEnumerator WaitForWaterSurface(StrippedTubBoatMovement boatMovement)
     {
-        while (boatMovement.targetSurface == null)
+        GameObject ocean = GameObject.Find("Ocean");
+        if (ocean != null)
         {
-            GameObject ocean = GameObject.Find("Ocean");
-            if (ocean != null)
-            {
-                boatMovement.targetSurface = ocean.GetComponent<WaterSurface>();
-                if (boatMovement.targetSurface != null)
-                {
-                    Debug.Log("WaterSurface found on Ocean object: " + ocean.name);
-                    yield break;
-                }
-            }
-
-            boatMovement.targetSurface = FindObjectOfType<WaterSurface>();
+            boatMovement.targetSurface = ocean.GetComponent<WaterSurface>();
             if (boatMovement.targetSurface != null)
             {
-                Debug.Log("WaterSurface found via FindObjectOfType on: " + boatMovement.targetSurface.gameObject.name);
+                Debug.Log("WaterSurface found on Ocean object: " + ocean.name);
                 yield break;
             }
-
-            yield return new WaitForSeconds(1f);
         }
+        boatMovement.targetSurface = FindObjectOfType<WaterSurface>();
+        if (boatMovement.targetSurface != null)
+        {
+            Debug.Log("WaterSurface found via FindObjectOfType on: " + boatMovement.targetSurface.gameObject.name);
+        }
+        else
+        {
+            Debug.LogError("WaterSurface component not found in the scene.", this);
+        }
+        yield break;
     }
 
     private void OnDestroy()
     {
         if (NetworkManager.Singleton != null)
-            NetworkManager.Singleton.OnClientConnectedCallback -= SpawnForClient;
+            NetworkManager.Singleton.SceneManager.OnSceneEvent -= HandleSceneEvent;
     }
 }
