d889e3829db9eec66055990e0da1e7d6f23ffcf5
TherealRioux <justinrioux0@outlook.com>
2025-06-02 08:41:49 -0700

    re-added the Complete Level 1 Missions text + Triggers to the scene (they just disappeared for some reason)

--- Assets/_Scripts/NetPlayerTracking/LevelProgressionManager.cs
+++ Assets/_Scripts/NetPlayerTracking/LevelProgressionManager.cs
@@ -8,6 +8,19 @@ public class LevelProgressionManager : NetworkBehaviour
 
     [Tooltip("Stores the Levels Goals Messages")]
     [SerializeField] private List<string> missionGoals = new();
+    /*   Mission Description for Clipboard:
+     *   Mission #1-1: Find a way out from the basin deep enough for the barge to pass through.
+     *   Mission #1-1: Use the buoys to navigate to the next area.
+     *   Mission #2: Get past the Debris field without taking to much damage!
+     *   Mission #3: Navigate through the whirlpool zone as fast as possible!
+     *   Mission #4: Make your way to the entrance of the sand dune corridor by going left.
+     *   
+     *   Mission #5: Work together to get the barge through the narrow sand dune corridor without getting stuck!
+     *   or 
+     *   Mission #5: Work together to get through the narrow dune corridor without getting stuck!  <--Fits better
+     *   
+     *   Mission #6: Bring the barge to the dock! Good work out there!
+     */
 
     [SerializeField] private NetworkVariable<int> currentGoalIndex = new(0);
 
